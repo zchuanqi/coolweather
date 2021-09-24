6df0f01c2b29848f3907eb54690da05c49ba8e48
chuanqi <2294690684@qq.com>
2021-09-24 17:15:01 +0800

    新增切换城市和手动更新天气的功能。

--- app/src/main/java/com/coolweather/android/MainActivity.java
+++ app/src/main/java/com/coolweather/android/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,9 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        SharedPreferences prefs=getPreferences(MODE_PRIVATE);
-        if(prefs.getString("weather",null)!=null){
-            Intent intent=new Intent(this,WeatherActivity.class);
+        SharedPreferences prefs = getSharedPreferences("main", MODE_PRIVATE);
+        if (prefs.getString("weather", null) != null) {
+            Toast.makeText(this, "跳转", Toast.LENGTH_SHORT).show();
+            Intent intent = new Intent(this, WeatherActivity.class);
             startActivity(intent);
             finish();
         }
